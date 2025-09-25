8f55c3aac62ad69ed08b8db3655f7d6a98b2da85
JordyTshibssB <jordy@uney.com>
2025-09-25 10:58:26 +0400

    fix: correct typo in bgp-projects-showcase component

--- src/components/molecules/home/bgp-projects-showcase.tsx
+++ src/components/molecules/home/bgp-projects-showcase.tsx
@@ -21,17 +21,17 @@ function BGPProjectsShowCases() {
       >
         La géophysique est très utilisée comme outil de détection directe de
         gisements pour l'exploration minière. En effet, le contraste entre les
-        propriétés physi&apos;apos;ues spécifiques des gisements métallifères et
+        propriétés physiques spécifiques des gisements métallifères et
         l'encaissant rocheux crée des « anomalies » qui peuvent être détectées à
-        l&lsquo; surface du sol, ou en aéroporté. C'est le cas pour les
-        contrastes de susceptibilité magnétique, po&apos;apos;r la
-        de&apos;apos;apos;sité, la conductivité électrique et pour la
-        radioactivité naturelle (Thorium, Uranium et Potassium). BIGEMIP Sarl
-        met ces anomalies géophysiques en relation avec les formations
-        géologiques dans vos permis miniers et peut offrir les éléments
-        qualitatifs et quantitatifs (si les données sont disponibles) du modèle
-        géologique régional ou local susceptible de contenir une minéralisation
-        utile, avec des concentrations économiquement exploitables
+        la surface du sol, ou en aéroporté. C'est le cas pour les contrastes de
+        susceptibilité magnétique, pour la densité, la conductivité électrique
+        et pour la radioactivité naturelle (Thorium, Uranium et Potassium).
+        BIGEMIP Sarl met ces anomalies géophysiques en relation avec les
+        formations géologiques dans vos permis miniers et peut offrir les
+        éléments qualitatifs et quantitatifs (si les données sont disponibles)
+        du modèle géologique régional ou local susceptible de contenir une
+        minéralisation utile, avec des concentrations économiquement
+        exploitables
       </motion.p>
     </section>
   );
